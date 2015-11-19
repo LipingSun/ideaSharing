dabe4234c5c4a5d8573cc60283a980d61adcf8fb
LipingSun <liping.sun@sjsu.edu>
2015-11-19 11:09:49 -0800

    Create UserControllerTest

---
+++ src/test/java/edu/sjsu/cmpe275/project/controller/UserControllerTest.java
@@ -0,0 +1,16 @@
+package edu.sjsu.cmpe275.project.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Liping on 11/19/15.
+ */
+public class UserControllerTest {
+
+    @Test
+    public void testGetUser() throws Exception {
+
+    }
+}
