516d988add68515ce141cb406ce6cd4781387570
Liping <Liping@MacBook-Pro.local>
2015-11-23 05:03:33 -0800

    Add resources utils

---
+++ src/main/java/edu/sjsu/cmpe275/project/rest/util/HeaderUtil.java
@@ -0,0 +1,29 @@
+package edu.sjsu.cmpe275.project.rest.util;
+
+import org.springframework.http.HttpHeaders;
+
+/**
+ * Utility class for http header creation.
+ *
+ */
+public class HeaderUtil {
+
+    public static HttpHeaders createAlert(String message, String param) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-ideaSharingPrototypeApp-alert", message);
+        headers.add("X-ideaSharingPrototypeApp-params", param);
+        return headers;
+    }
+
+    public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
+        return createAlert("A new " + entityName + " is created with identifier " + param, param);
+    }
+
+    public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
+        return createAlert("A " + entityName + " is updated with identifier " + param, param);
+    }
+
+    public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
+        return createAlert("A " + entityName + " is deleted with identifier " + param, param);
+    }
+}
