065215600044ba19126d038f1fcb6d0cfdf6f6d4
Xiaotong Wang <xiaotong.wang.estelle@gmail.com>
2015-11-25 15:12:15 -0800

    UserRepository Test Save

---
+++ src/test/java/edu/sjsu/cmpe275/project/User_and_Idea_Test/IdeaTest.java
@@ -0,0 +1,25 @@
+package edu.sjsu.cmpe275.project.User_and_Idea_Test;
+
+import edu.sjsu.cmpe275.project.rest.IdeaResource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ * Created by xiaotong on 11/25/15.
+ */
+public class IdeaTest {
+    private final MockMvc mockMvc = standaloneSetup(new IdeaResource()).build();
+
+    @org.junit.Test
+    public void testGetIdeas() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("api/ideas/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("title").value("Good Idea"));
+    }
+}
--- src/test/java/edu/sjsu/cmpe275/project/User_and_Idea_Test/UserTest.java
+++ src/test/java/edu/sjsu/cmpe275/project/User_and_Idea_Test/UserTest.java
@@ -1,19 +1,27 @@
 package edu.sjsu.cmpe275.project.User_and_Idea_Test;
 
+import edu.sjsu.cmpe275.project.rest.IdeaResource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 /**
  * Created by xiaotong on 11/25/15.
  */
-public class Test {
-    public void testGetIdeas() throws Exception{
-//        mockMvc.perform(MockMvcRequestBuilders.get("api/v1/users/1/ideas"))
+public class UserTest {
+    private final MockMvc mockMvc = standaloneSetup(new IdeaResource()).build();
+
+//    @org.junit.Test
+//    public void testGetIdeas() throws Exception{
+//        mockMvc.perform(MockMvcRequestBuilders.get("api/ideas/1"))
 //                .andDo(print())
 //                .andExpect(status().isOk())
 //                .andExpect(jsonPath("title").value("Good Idea"));
-    }
+//    }
+
+
 }
