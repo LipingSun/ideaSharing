1f4b44f702d39dbfa4bffea7e304050a63c94668
catherineccc <chang.xue@sjsu.edu>
2015-11-27 14:42:50 -0800

    add getLikedIdeas() and getUsers()

--- src/main/java/edu/sjsu/cmpe275/project/rest/UserLikedIdeaResource.java
+++ src/main/java/edu/sjsu/cmpe275/project/rest/UserLikedIdeaResource.java
@@ -1,7 +1,11 @@
 package edu.sjsu.cmpe275.project.rest;
 
+import edu.sjsu.cmpe275.project.domain.Idea;
+import edu.sjsu.cmpe275.project.domain.User;
 import edu.sjsu.cmpe275.project.domain.UserLikedIdea;
+import edu.sjsu.cmpe275.project.repository.IdeaRepository;
 import edu.sjsu.cmpe275.project.repository.UserLikedIdeaRepository;
+import edu.sjsu.cmpe275.project.repository.UserRepository;
 import edu.sjsu.cmpe275.project.rest.util.HeaderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * REST controller for managing UserLikedIdea.
+ * REST controller for managing UserLikedIdea. Chang's part
  */
 @RestController
 @RequestMapping("/api")
@@ -28,9 +32,13 @@ public class UserLikedIdeaResource {
     @Inject
     private UserLikedIdeaRepository userLikedIdeaRepository;
 
-    /**
-     * POST  /userLikedIdeas -> Create a new userLikedIdea.
-     */
+    @Inject
+    private UserRepository userRepository;
+
+    @Inject
+    private IdeaRepository ideaRepository;
+
+
     @RequestMapping(value = "/userLikedIdeas",
         method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_VALUE)
@@ -77,7 +85,7 @@ public class UserLikedIdeaResource {
     }
 
     /**
-     * GET  /userLikedIdeas/:id -> get the "id" userLikedIdea.
+     * GET  /userLikedIdeas/:idea_id -> get the "idea_id" userLikedIdea.
      */
     @RequestMapping(value = "/userLikedIdeas/{id}",
         method = RequestMethod.GET,
@@ -93,7 +101,44 @@ public class UserLikedIdeaResource {
     }
 
     /**
-     * DELETE  /userLikedIdeas/:id -> delete the "id" userLikedIdea.
+     * GET  /users/{user_id}/like -> get all ideas from a specific user through user_id.
+     */
+    @RequestMapping(value = "users/{user_id}/like",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public List<UserLikedIdea> getLikedIdeas(@PathVariable Long user_id) {
+        log.debug("REST request to get user_id based UserLikedIdea : {}", user_id);
+        User user = userRepository.findOne(user_id);
+        return userLikedIdeaRepository.findByUser(user);
+    }
+
+
+    /**
+     * GET  /ideas/{idea_id}/like -> get all users those like a specific idea through idea_id.
+     */
+    @RequestMapping(value = "ideas/{idea_id}/like",
+                    method = RequestMethod.GET,
+                    produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public List<UserLikedIdea> getUsers(@PathVariable Long idea_id) {
+        log.debug("REST request to get idea_id based UserLikedIdea :{}", idea_id);
+        Idea idea = ideaRepository.findOne(idea_id);
+        return userLikedIdeaRepository.findByIdea(idea);
+    }
+
+
+    /**
+     * PUT  /like/{idea_id}/{user_id} -> create an entry with a specific pair of idea_id and user_id.
+     */
+
+    /**
+     * DELETE  /like/{idea_id}/{user_id} -> Delete an entry with a specific pair of idea_id and user_id.
+     */
+
+
+    /**
+     * DELETE  /userLikedIdeas/:idea_id -> delete the "idea_id" userLikedIdea.
      */
     @RequestMapping(value = "/userLikedIdeas/{id}",
         method = RequestMethod.DELETE,
