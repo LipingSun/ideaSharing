ce1a1e092f776304e10531ed6b2760d3669b9fac
catherineccc <chang.xue@sjsu.edu>
2015-11-27 14:39:11 -0800

    add findByUser(User user) and findByIdea(Idea idea)

--- src/main/java/edu/sjsu/cmpe275/project/repository/UserLikedIdeaRepository.java
+++ src/main/java/edu/sjsu/cmpe275/project/repository/UserLikedIdeaRepository.java
@@ -1,11 +1,23 @@
 package edu.sjsu.cmpe275.project.repository;
 
+import edu.sjsu.cmpe275.project.domain.Idea;
+import edu.sjsu.cmpe275.project.domain.User;
 import edu.sjsu.cmpe275.project.domain.UserLikedIdea;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Spring Data JPA repository for the UserLikedIdea entity.
- */
-public interface UserLikedIdeaRepository extends JpaRepository<UserLikedIdea,Long> {
+import java.util.List;
 
+/**
+ * Spring Data JPA repository for the UserLikedIdea entity. Chang's part
+ */
+public interface UserLikedIdeaRepository extends JpaRepository<UserLikedIdea, Long> {
+    /**
+     * Returns the UserLikedIdea entry list by using its user as search criteria.
+     */
+    List<UserLikedIdea> findByUser(User user);
+
+    /**
+     * Returns the UserLikedIdea entry list by using its idea as search criteria.
+     */
+    List<UserLikedIdea> findByIdea(Idea idea);
 }
