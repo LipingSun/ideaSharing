0f5ed97028653de35bd1d1cdb3c1100758f86876
LipingSun <liping.sun@sjsu.edu>
2015-11-28 23:52:26 -0800

    Init frontend framework

---
+++ src/main/webapp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ideaSharingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ideaSharingApp
+ */
+angular.module('ideaSharingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
