974306b960ada9b9d123f3704bcb2312ca250369
LipingSun <liping.sun@sjsu.edu>
2015-12-01 16:47:35 -0800

    Add home and idea

---
+++ src/main/webapp/app/scripts/controllers/home.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ideaSharingApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the ideaSharingApp
+ */
+angular.module('ideaSharingApp')
+  .controller('HomeCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ src/main/webapp/app/scripts/controllers/idea.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ideaSharingApp.controller:IdeaCtrl
+ * @description
+ * # IdeaCtrl
+ * Controller of the ideaSharingApp
+ */
+angular.module('ideaSharingApp')
+  .controller('IdeaCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
