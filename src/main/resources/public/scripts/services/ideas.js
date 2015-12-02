0f36b7d118bd4a7284b8a384786415abb0dc547f
LipingSun <liping.sun@sjsu.edu>
2015-12-02 01:00:15 -0800

    Implement idea resource service

---
+++ src/main/resources/public/scripts/services/ideas.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ideaSharingApp.ideas
+ * @description
+ * # ideas
+ * Service in the ideaSharingApp.
+ */
+angular.module('ideaSharingApp')
+    .service('ideas', function ($resource) {
+      return $resource('http://localhost:8080/api/ideas/:id', {}, {})
+    });
