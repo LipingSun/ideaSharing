ffdf4d8d7de64086725c2e51b2cfd455e74fdbb3
LipingSun <liping.sun@sjsu.edu>
2015-12-01 22:36:01 -0800

    Support CORS

---
+++ src/main/java/edu/sjsu/cmpe275/project/config/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package edu.sjsu.cmpe275.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Liping on 12/1/15.
+ */
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+        };
+    }
+}
