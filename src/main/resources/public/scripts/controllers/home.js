82e9dd756ff1bf1ade342f18f3de2d0cd9da0d9f
LipingSun <liping.sun@sjsu.edu>
2015-12-02 00:58:43 -0800

    Implement home page controller

--- src/main/resources/public/scripts/controllers/home.js
+++ src/main/resources/public/scripts/controllers/home.js
@@ -8,10 +8,19 @@
  * Controller of the ideaSharingApp
  */
 angular.module('ideaSharingApp')
-  .controller('HomeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('HomeCtrl', function (ideas) {
+
+        var ctrl = this;
+
+        ctrl.ideas = ideas.query(function () {
+            ctrl.ideas.forEach(function (idea) {
+                var date = new Date(idea.datetime * 1000);
+                idea.datetimeString = monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+            })
+        });
+
+    });
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
