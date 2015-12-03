4de17d01defb24e278313a663a2676b9f8f54c65
LipingSun <liping.sun@sjsu.edu>
2015-12-02 16:32:33 -0800

    Implement comments resource

---
+++ src/main/resources/public/scripts/services/comments.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ideaSharingApp.comments
+ * @description
+ * # comments
+ * Service in the ideaSharingApp.
+ */
+angular.module('ideaSharingApp')
+    .service('comments', function ($resource) {
+        return $resource('http://localhost:8080/api/ideas/:idea_id/comments/:id', {}, {})
+    });
