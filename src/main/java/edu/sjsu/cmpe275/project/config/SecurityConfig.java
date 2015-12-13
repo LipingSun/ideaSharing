f9da1bbb21640224536550b5ce862f5ee90d981c
LipingSun <liping.sun@sjsu.edu>
2015-12-12 17:32:43 -0800

    Enable all APIs without security

--- src/main/java/edu/sjsu/cmpe275/project/config/SecurityConfig.java
+++ src/main/java/edu/sjsu/cmpe275/project/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf()
                 .disable()
             .authorizeRequests()
-                .antMatchers("/api/ideas").permitAll()
+                .antMatchers("/api**").permitAll()
                 .and()
             .formLogin()
                 .loginPage("/login")
