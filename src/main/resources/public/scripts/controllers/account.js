d28ddeb6c98c6f36b709881f753d22668c019997
J Gao <chienshin1@gmail.com>
2015-12-12 17:28:12 -0800

    add account page

---
+++ src/main/resources/public/scripts/controllers/account.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ideaSharingApp.controller:AccountCtrl
+ * @description
+ * # AccountCtrl
+ * Controller of the ideaSharingApp
+ */
+angular.module('ideaSharingApp')
+    .controller('AccountCtrl', function (users) {
+
+        var ctrl = this;
+
+        var user = users.get({id: "1"}, function () {
+            console.log(user);
+
+            ctrl.user = user;
+
+        });
+
+
+
+        //ctrl.submitComment = function () {
+        //    var newComment = new comments(ctrl.idea.id, ctrl.userComment);
+        //    newComment.$save();
+        //}
+
+
+    });
